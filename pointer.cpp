74aeba5c95f22b4ca3c1acd885a06ce1014d7c67
Saurabh98719 <ksaurabh98719@gmail.com>
2026-08-03 19:39:38 +0530

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+// #include <string>
+// #include <cctype> // for tolower
+// using namespace std;
+
+// bool isPalindrome(string s) {
+//     int i = 0, j = s.length() - 1;
+
+//     while(i < j) {
+//         // convert to lowercase for fair check: Madam = madam
+//         if(tolower(s[i])!= tolower(s[j])) {
+//             return false;
+//         }
+//         i++;
+//         j--;
+//     }
+//     return true;
+// }
+
+// int main() {
+//     string name;
+//     cout << "Enter your name: ";
+//     getline(cin, name); 
+
+//     if(isPalindrome(name)) {
+//         cout << name << " is a Palindrome " << endl;
+//     } else {
+//         cout << name << " is NOT a Palindrome " << endl;
+//     }
+
+//     return 0;
+// }
+
